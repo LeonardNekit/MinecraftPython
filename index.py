46e0f6e8e7456d5ac684e0d0f225cd729ec659b7
Некит <141399952+LeonardNekit@users.noreply.github.com>
2024-01-05 13:14:19 +0300

    Сам лаунчер

---
+++ index.py
@@ -0,0 +1,47 @@
+# библиотеки
+try:
+    import minecraft_launcher_lib
+except:
+    print('У вас нет библиотеки minecraft_launcher_lib, пожалуйста скачайте данную библиотеку через pip')
+import subprocess
+
+minecraft_directory = minecraft_launcher_lib.utils.get_minecraft_directory().replace('minecraft', 'chto_za_zalupa')
+
+print('Привет')
+print('Ты попал в лаунчер для майна, здесь нет реклам, доп. контентов и интерфейса для слабых устройства')
+print('Для скачивания, потребуется java se!')
+
+# функции для упрощения
+def printProgressBar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█', printEnd="\r"):
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end=printEnd)
+    if iteration == total:
+        print()
+
+def maximum(max_value, value):
+    max_value[0] = value
+
+version = input('Версия: ')
+username = input('НикНейм: ')
+
+print('=======================================================================================')
+
+# действие устройства
+
+max_value = [0]
+
+callback = {
+        "setStatus": lambda text: print(text, end='r'),
+        "setProgress": lambda value: printProgressBar(value, max_value[0]),
+        "setMax": lambda value: maximum(max_value, value)
+}
+
+minecraft_launcher_lib.install.install_minecraft_version(versionid=version, minecraft_directory=minecraft_directory, callback=callback)
+
+options = {
+    'username': username,
+}
+
+subprocess.call(minecraft_launcher_lib.command.get_minecraft_command(version=version, minecraft_directory=minecraft_directory, options=options))
